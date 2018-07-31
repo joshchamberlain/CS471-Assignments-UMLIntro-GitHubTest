c96bcd30b4fcd73dd22b7b019b69fd674ba48fe0
Josh <joshchamberlain@u.boisestate.edu>
2018-07-31 17:02:46 -0600

    Updating junit configuration, adding a test that passes

--- src/test/java/edu/boisestate/cs471/model/SortingAlgorithmResultsTest.java
+++ src/test/java/edu/boisestate/cs471/model/SortingAlgorithmResultsTest.java
@@ -1,25 +1,34 @@
 package edu.boisestate.cs471.model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-import edu.boisestate.cs471.model.Model;
-
-@RunWith(Parameterized.class)
 public class SortingAlgorithmResultsTest {
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        Model m = new Model();
+
     }
 
     @Test
     public final void testFoo() {
         fail("Not yet implemented"); // TODO
+    }
+
+    public final void testBubbleSort() {
+        int n = 10;
+        BubbleSort sort = new BubbleSort(10);
+        sort.randomize();
+        for (int i = 0; i < (n*n); i++) {
+            sort.doSortIteration();
+            if (sort.isSorted()) {
+                break;
+            }
+        }
+        assertTrue(sort.isSorted());
     }
 
 }
