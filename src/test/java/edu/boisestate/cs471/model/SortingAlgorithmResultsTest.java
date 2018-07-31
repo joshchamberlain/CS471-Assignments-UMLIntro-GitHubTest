2aa66a3d2aa2994b0a48821c34e359fa4659b696
Josh <joshchamberlain@u.boisestate.edu>
2018-07-31 16:55:45 -0600

    Converting to maven and adding a unit test

---
+++ src/test/java/edu/boisestate/cs471/model/SortingAlgorithmResultsTest.java
@@ -0,0 +1,25 @@
+package edu.boisestate.cs471.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import edu.boisestate.cs471.model.Model;
+
+@RunWith(Parameterized.class)
+public class SortingAlgorithmResultsTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        Model m = new Model();
+    }
+
+    @Test
+    public final void testFoo() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
